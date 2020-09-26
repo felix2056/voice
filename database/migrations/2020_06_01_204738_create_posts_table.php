1d25fc0be647902e2d801d944cb5c7b3b438cbae
felix7595 <felixdaniel2056@gmail.com>
2020-09-26 22:29:37 +0100

    change avatar get name to get username

--- database/migrations/2020_06_01_204738_create_posts_table.php
+++ database/migrations/2020_06_01_204738_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
-            $table->string('headline');
+            $table->text('headline');
             $table->timestamps();
         });
     }
